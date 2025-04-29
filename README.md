84dbead51f784e661463531f2dfd93097038e57d
Running water <alsgks1218@naver.com>
2025-04-29 15:48:24 +0900

    리드미에 기본 구조 설명

--- README.md
+++ README.md
@@ -11,3 +11,29 @@ vite로 바닐라 자바스크립트 프로젝트를 만들고 거기에 아래 
 ```bash
 npm i pixi.js
 ```
+
+## 기본 구조
+
+```javascript
+import * as PIXI from "pixi.js";
+
+(async () => {
+  const app = new PIXI.Application();
+
+  # 초기화 함수
+  await app.init({
+    background: "royalblue",
+  });
+
+  # 아이디 설정
+  # 다른 옵션도 들어갈 수 있는 것 같음
+  app.canvas.id = "app-canvas";
+
+  document.body.appendChild(app.canvas);
+})();
+```
+
+기본적으로 모듈 PIXI에서 Application을 불러와서 app에 클래스를 생성한다.  
+그리고 init을 통해 초기화할 수 있다. init은 비동기 함수로 await를 사용해야 하기 때문에 즉시 실행함수를 통해서 구현하는게 기본적인 구조가 된다.
+
+id 등 캔버스에 관련된 옵션도 설정하고 body에 캔버스를 app.canvas를 통해 만들어진 캔버스를 전달해주면 기본 구조가 된다.
--- src/main.js
+++ src/main.js
@@ -1,24 +1,15 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import * as PIXI from "pixi.js";
 
-setupCounter(document.querySelector('#counter'))
+(async () => {
+  const app = new PIXI.Application();
+
+  await app.init({
+    background: "royalblue",
+  });
+
+  app.canvas.id = "app-canvas";
+
+  document.body.appendChild(app.canvas);
+})();
